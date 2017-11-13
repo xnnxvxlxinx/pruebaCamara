c76dfd95036444bd5a3786f4b85c996cee331c6f
xnnxvxlxinx <32514087+xnnxvxlxinx@users.noreply.github.com>
2017-11-12 20:34:01 -0500

    6

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,14 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
-    "id": "cordova-plugin-device.device",
-    "file": "plugins/cordova-plugin-device/www/device.js",
-    "pluginId": "cordova-plugin-device",
-    "clobbers": [
-      "device"
-    ]
-  },
-  {
     "id": "cordova-plugin-camera.Camera",
     "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
     "pluginId": "cordova-plugin-camera",
@@ -212,32 +204,14 @@ module.exports = [
       "cordova"
     ],
     "runs": true
-  },
-  {
-    "id": "cordova-plugin-file-transfer.FileTransferError",
-    "file": "plugins/cordova-plugin-file-transfer/www/FileTransferError.js",
-    "pluginId": "cordova-plugin-file-transfer",
-    "clobbers": [
-      "window.FileTransferError"
-    ]
-  },
-  {
-    "id": "cordova-plugin-file-transfer.FileTransfer",
-    "file": "plugins/cordova-plugin-file-transfer/www/FileTransfer.js",
-    "pluginId": "cordova-plugin-file-transfer",
-    "clobbers": [
-      "window.FileTransfer"
-    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-device": "1.1.6",
-  "cordova-plugin-camera": "3.0.0",
-  "cordova-plugin-file": "5.0.0",
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-file-transfer": "1.7.0"
+  "cordova-plugin-camera": "3.0.0",
+  "cordova-plugin-file": "5.0.0"
 };
 // BOTTOM OF METADATA
 });
