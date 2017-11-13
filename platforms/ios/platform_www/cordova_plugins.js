184b0e5df60e79e5cd4df1f787295c3932640c9c
xnnxvxlxinx <32514087+xnnxvxlxinx@users.noreply.github.com>
2017-11-12 19:38:49 -0500

    7

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -212,6 +212,22 @@ module.exports = [
       "cordova"
     ],
     "runs": true
+  },
+  {
+    "id": "cordova-plugin-file-transfer.FileTransferError",
+    "file": "plugins/cordova-plugin-file-transfer/www/FileTransferError.js",
+    "pluginId": "cordova-plugin-file-transfer",
+    "clobbers": [
+      "window.FileTransferError"
+    ]
+  },
+  {
+    "id": "cordova-plugin-file-transfer.FileTransfer",
+    "file": "plugins/cordova-plugin-file-transfer/www/FileTransfer.js",
+    "pluginId": "cordova-plugin-file-transfer",
+    "clobbers": [
+      "window.FileTransfer"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -221,7 +237,8 @@ module.exports.metadata =
   "cordova-plugin-compat": "1.2.0",
   "cordova-plugin-camera": "2.4.1",
   "cordova-plugin-device": "1.1.6",
-  "cordova-plugin-file": "5.0.0"
+  "cordova-plugin-file": "5.0.0",
+  "cordova-plugin-file-transfer": "1.7.0"
 };
 // BOTTOM OF METADATA
 });
