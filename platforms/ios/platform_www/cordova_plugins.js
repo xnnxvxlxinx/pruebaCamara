2f0cbdaaf93bdaf61b6966e1b7ba124370c9badb
xnnxvxlxinx <32514087+xnnxvxlxinx@users.noreply.github.com>
2017-11-14 12:25:51 -0500

    9

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -212,22 +212,6 @@ module.exports = [
     "clobbers": [
       "device"
     ]
-  },
-  {
-    "id": "cordova-plugin-file-transfer.FileTransferError",
-    "file": "plugins/cordova-plugin-file-transfer/www/FileTransferError.js",
-    "pluginId": "cordova-plugin-file-transfer",
-    "clobbers": [
-      "window.FileTransferError"
-    ]
-  },
-  {
-    "id": "cordova-plugin-file-transfer.FileTransfer",
-    "file": "plugins/cordova-plugin-file-transfer/www/FileTransfer.js",
-    "pluginId": "cordova-plugin-file-transfer",
-    "clobbers": [
-      "window.FileTransfer"
-    ]
   }
 ];
 module.exports.metadata = 
@@ -237,8 +221,7 @@ module.exports.metadata =
   "cordova-plugin-camera": "3.0.0",
   "cordova-plugin-file": "5.0.0",
   "cordova-plugin-device": "1.1.7",
-  "cordova-plugin-compat": "1.2.0",
-  "cordova-plugin-file-transfer": "1.7.0"
+  "cordova-plugin-compat": "1.2.0"
 };
 // BOTTOM OF METADATA
 });
