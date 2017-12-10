96a3afe3abff599f4df5888ff63ebd1296ef95e2
xnnxvxlxinx <32514087+xnnxvxlxinx@users.noreply.github.com>
2017-12-09 23:41:33 -0500

    j

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -244,33 +244,6 @@ module.exports = [
     "clobbers": [
       "window.FileTransfer"
     ]
-  },
-  {
-    "id": "cordova-plugin-local-notification.LocalNotification",
-    "file": "plugins/cordova-plugin-local-notification/www/local-notification.js",
-    "pluginId": "cordova-plugin-local-notification",
-    "clobbers": [
-      "cordova.plugins.notification.local",
-      "plugin.notification.local"
-    ]
-  },
-  {
-    "id": "cordova-plugin-local-notification.LocalNotification.Core",
-    "file": "plugins/cordova-plugin-local-notification/www/local-notification-core.js",
-    "pluginId": "cordova-plugin-local-notification",
-    "clobbers": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
-  },
-  {
-    "id": "cordova-plugin-local-notification.LocalNotification.Util",
-    "file": "plugins/cordova-plugin-local-notification/www/local-notification-util.js",
-    "pluginId": "cordova-plugin-local-notification",
-    "merges": [
-      "cordova.plugins.notification.local.core",
-      "plugin.notification.local.core"
-    ]
   }
 ];
 module.exports.metadata = 
@@ -281,9 +254,7 @@ module.exports.metadata =
   "cordova-plugin-device": "1.1.7",
   "cordova-plugin-camera": "2.4.1",
   "cordova-plugin-file": "4.3.3",
-  "cordova-plugin-file-transfer": "1.6.3",
-  "cordova-plugin-app-event": "1.2.1",
-  "cordova-plugin-local-notification": "0.8.4"
+  "cordova-plugin-file-transfer": "1.6.3"
 };
 // BOTTOM OF METADATA
 });
